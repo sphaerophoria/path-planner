229143a4f03e9b3762131f02af6c75113cb7b78e
Mick Sayson <mick@sayson.com>
2023-02-05 18:01:18 -0800

    Render selected way info on mouse hover
    While this is very cool, there are real reasons to do this. In order to
    find a sane path finding algorithm for walking, we have to understand
    what our data looks like. One area that is not clear to me at the moment
    is how the different way tags actually relate to areas that I can and
    can not access. Without a way to tie the data back to the map, there is
    no quick way to catch up here.
    
    Find the closest way to the mouse, and start adding info about it to the
    overlay. Color in the selected way as well to make it clear that we are
    reading the correct info.
    
    Implementation is a little hairy, see comments in commit for a better
    idea of what is happening

--- www/index.js
+++ www/index.js
@@ -1,44 +1,102 @@
 // Each degree should be scale steps 
 const VERTEX_SHADER_SOURCE =
-    `attribute vec2 long_lat; \
+    `#version 300 es
+     precision highp float;
+     in vec2 long_lat; \
+     in float way_id; \
+     out float f_way_id; \
+     out float f_selected_way; \
      uniform float scale; \
      uniform vec2 center; \
      uniform float aspect_ratio; \ 
+     uniform float selected_way; \
 
      void main(void) { \
          float lat_rad = long_lat.y * 3.1415962 / 180.0; \
          vec2 pos = (long_lat - center) * scale; \
          pos.x = pos.x * cos(lat_rad) / aspect_ratio; \
          gl_Position = vec4(pos, 0.1, 1.0); \
+         f_way_id = way_id; \
+         f_selected_way = selected_way; \
      }`
 
 const FRAGMENT_SHADER_SOURCE =
-    `void main(void) { \
-      gl_FragColor = vec4(1.0, 1.0, 1.0, 0.1); \
-    }`
+    `#version 300 es
+     precision highp float; \
+     in float f_way_id; \
+     in float f_selected_way; \
+     out vec4 fragColor; \
+     void main(void) { \
+       if (abs(f_way_id - f_selected_way) < 0.1) {
+           fragColor = vec4(1.0, 0.0, 0.0, 1.0); \
+       } else { \
+           fragColor = vec4(1.0, 1.0, 1.0, 1.0); \
+       } \
+     }`
 
-function _construct_index_buffer(ways) {
-    let total_size = 0
-    for (let i = 0; i < ways.length; i++) {
-        total_size += ways[i].nodes.length
-        total_size += 1
+const WAY_FINDER_FRAG_SOURCE =
+    `#version 300 es
+     precision highp float;
+     in float f_way_id; \
+     out vec4 fragColor; \
+     void main(void) { \
+       int full_id = int(f_way_id); \
+       // This is some abuse. readPixels needs an RGBA/uint8 output, so we just
+       // coerce our 32 bit int into it
+       float scale = float(0xff); \
+       fragColor = vec4( \
+           float((full_id >> 24) & 0xff) / scale,
+           float((full_id >> 16) & 0xff) / scale,
+           float((full_id >> 8) & 0xff) / scale,
+           float((full_id >> 0) & 0xff) / scale
+       ); \
+     }`
+
+function _constructMapBuffers(data) {
+    // vertex buffer has a unique vertex for shared node ids in a way. This
+    // allows us to attach info about a way to the render. E.g. color for
+    // different tag types. We still need to use an index buffer though, that
+    // essentially just is just a linear increase split by 0xffff. This allows
+    // us to use glDrawElements with primitive restart and draw the whole map
+    // in a single draw call
+
+    let index_size = 0
+    let vertex_size = 0
+    for (let i = 0; i < data.ways.length; i++) {
+        index_size += data.ways[i].nodes.length
+        index_size += 1
+        vertex_size += data.ways[i].nodes.length * 3
     }
 
-    let indices = new Uint32Array(total_size)
+    let indices = new Uint32Array(index_size)
+    let vertices = new Float32Array(vertex_size)
 
-    let it = 0
-    for (const way of ways) {
+    let vertex_it = 0
+    let index_it = 0
+    for (let i = 0; i < data.ways.length; i++) {
+        let way = data.ways[i]
+
         for (let j = 0; j < way.nodes.length; j++) {
-            indices[it + j] = way.nodes[j]
+            let node_id = way.nodes[j]
+            // Data is in decimicro degrees, but we just convert to lower
+            // precision floats because it's easier  to think about, has good
+            // interop with webgl, and doesn't matter
+            vertices[vertex_it * 3] = data.nodes[node_id].long / 10000000.0
+            vertices[vertex_it * 3 + 1] = data.nodes[node_id].lat / 10000000.0
+            vertices[vertex_it * 3 + 2] = i
+            indices[index_it] = vertex_it
+            vertex_it += 1
+            index_it += 1
         }
 
         // webgl2 enables primitive restart index by default
-        indices[it + way.nodes.length] = 0xffffffff
-        it += way.nodes.length + 1
+        indices[index_it] = 0xffffffff
+        index_it += 1
     }
 
-    return indices
+    return [vertices, indices]
 }
+
 
 class Renderer {
     constructor(data) {
@@ -48,18 +106,8 @@ class Renderer {
         let gl = this.gl
         this.scale = 10.0
         this.center = [-123.1539434, 49.2578263]
-
-        let vertices = new Float32Array(data.nodes.length * 2)
-        for (let i = 0; i < data.nodes.length; i++) {
-            const node = data.nodes[i]
-            vertices[i * 2] = node.long / 10000000.0
-            vertices[i * 2 + 1] = node.lat / 10000000.0
-        }
-
-        this.vertex_buffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertex_buffer);
-        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+        this.data = data
+        this.selected_way_id = -1
 
         let vertShader = gl.createShader(gl.VERTEX_SHADER);
         gl.shaderSource(vertShader, VERTEX_SHADER_SOURCE);
@@ -74,12 +122,37 @@ class Renderer {
         gl.attachShader(this.shaderProgram, fragShader);
         gl.linkProgram(this.shaderProgram);
 
-        let indices = _construct_index_buffer(data.ways)
+        let wayFinderShader = gl.createShader(gl.FRAGMENT_SHADER);
+        gl.shaderSource(wayFinderShader, WAY_FINDER_FRAG_SOURCE);
+        gl.compileShader(wayFinderShader);
+
+        this.wayFinderProgram = gl.createProgram()
+        gl.attachShader(this.wayFinderProgram, vertShader);
+        gl.attachShader(this.wayFinderProgram, wayFinderShader);
+        gl.linkProgram(this.wayFinderProgram);
+
+        this.wayFinderTexture = gl.createRenderbuffer();
+        gl.bindRenderbuffer(gl.RENDERBUFFER, this.wayFinderTexture)
+        gl.renderbufferStorage(gl.RENDERBUFFER, gl.RGBA8, 2, 2)
+
+        this.wayFinderBuffer = gl.createFramebuffer();
+        gl.bindFramebuffer(gl.FRAMEBUFFER, this.wayFinderBuffer)
+        gl.framebufferRenderbuffer(gl.DRAW_FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.RENDERBUFFER, this.wayFinderTexture)
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+        let [vertices, indices] = _constructMapBuffers(data)
+
+        this.vertex_buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertex_buffer);
+        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
         this.index_buffer = gl.createBuffer();
         this.index_buffer_length = indices.length;
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.index_buffer);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null)
+
 
         let canvasHolder = document.getElementById('canvas-holder')
         canvasHolder.onmousemove = this.onMouseMove.bind(this)
@@ -109,6 +182,9 @@ class Renderer {
         let aspect_ratio_loc = gl.getUniformLocation(this.shaderProgram, "aspect_ratio");
         gl.uniform1f(aspect_ratio_loc, this.canvas.width / this.canvas.height)
 
+        let selected_way_loc = gl.getUniformLocation(this.shaderProgram, "selected_way");
+        gl.uniform1f(selected_way_loc, this.selected_way_id)
+
         gl.clearColor(0.5, 0.5, 0.5, 0.9);
         gl.viewport(0, 0, this.canvas.width, this.canvas.height);
         gl.clear(gl.COLOR_BUFFER_BIT);
@@ -116,7 +192,7 @@ class Renderer {
         gl.bindBuffer(gl.ARRAY_BUFFER, this.vertex_buffer);
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.index_buffer);
 
-        gl.vertexAttribPointer(long_lat_loc, 2, gl.FLOAT, false, 0, 0);
+        gl.vertexAttribPointer(long_lat_loc, 2, gl.FLOAT, false, 12, 0);
         gl.enableVertexAttribArray(long_lat_loc);
 
         gl.drawElements(gl.LINE_STRIP, this.index_buffer_length, gl.UNSIGNED_INT, 0);
@@ -141,6 +217,7 @@ class Renderer {
     onMouseMove(e) {
         this._update_overlay(e)
         if (e.buttons == 0) {
+            window.requestAnimationFrame(this.render_map.bind(this))
             return
         }
 
@@ -156,11 +233,98 @@ class Renderer {
         window.requestAnimationFrame(this.render_map.bind(this))
     }
 
+    _findTagForLongLat(long, lat) {
+
+        // This is what I would consider to be a colossal hack
+        //
+        // In order to figure out which "way" is closest to us, we don't really
+        // have a choice but to check our proximity to every way. We could
+        // pre-process this data to try to create "cells" of ways that reduce
+        // the amount of checks, but there's a lot of complexity there.
+        //
+        // If we check every way in javascript, doing a simple axis aligned
+        // bbox check, it takes 174ms. That's completely unreasonable if we
+        // want to do it every frame or on every mouse move.
+        //
+        // The good news is we already render every single road on every frame,
+        // and that's incredibly fast. The GPU is handling everything for us.
+        // If we can somehow leverage that power in WebGL we get the answer
+        // we're looking for.
+        //
+        // So we...
+        // * Use our usual map drawing vertex shader, but we center the
+        //   "viewport" on a very zoomed in segment near the provided longitude
+        //   and latitude
+        // * We use a special fragment shader that just outputs the way id
+        //   directly
+        // * We render that scene to a 1x1 pixel render buffer. If there are
+        //   multiple ways in the viewport this should just pick one at random
+        // * We read back that single pixel, and do some bit shifting hackery
+        //   to extract a 32bit way id from a RGBA8 color
+        //
+        // This takes around 1.2ms on machine vs the 174 I tested with the
+        // simple javascript approach
+
+        let gl = this.gl;
+        gl.useProgram(this.wayFinderProgram);
+
+        let long_lat_loc = gl.getAttribLocation(this.wayFinderProgram, "long_lat");
+        let way_id_loc = gl.getAttribLocation(this.wayFinderProgram, "way_id");
+
+        let scale_loc = gl.getUniformLocation(this.wayFinderProgram, "scale");
+        gl.uniform1f(scale_loc, this.scale * 50)
+
+        let center_loc = gl.getUniformLocation(this.wayFinderProgram, "center");
+        gl.uniform2f(center_loc, long, lat)
+
+        let aspect_ratio_loc = gl.getUniformLocation(this.wayFinderProgram, "aspect_ratio");
+        gl.uniform1f(aspect_ratio_loc, this.canvas.width / this.canvas.height)
+
+        gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, this.wayFinderBuffer)
+
+        gl.clearColor(1.0, 1.0, 1.0, 1.0);
+        gl.viewport(0, 0, 1, 1);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertex_buffer);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.index_buffer);
+
+        gl.vertexAttribPointer(long_lat_loc, 2, gl.FLOAT, false, 12, 0);
+        gl.enableVertexAttribArray(long_lat_loc);
+
+        gl.vertexAttribPointer(way_id_loc, 1, gl.FLOAT, false, 12, 8);
+        gl.enableVertexAttribArray(way_id_loc);
+
+        gl.drawElements(gl.LINE_STRIP, this.index_buffer_length, gl.UNSIGNED_INT, 0);
+
+        // Duplicate vertices and assign a way ID
+        // Set the center of the image to be where the mouse is
+        // Render a 1x1 image with scale / 100
+        // Check "color" of the output
+        let pixels = new Uint8Array(4)
+        gl.bindFramebuffer(gl.READ_FRAMEBUFFER, this.wayFinderBuffer)
+        gl.readPixels(0, 0, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, pixels)
+        gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, null);
+
+        let way_id = (pixels[0] << 24 | pixels[1] << 16 | pixels[2] << 8 | pixels[3])
+        return way_id
+    }
 
     _update_overlay(e) {
         let [x, y] = this._pixelToLongLat(e.pageX, e.pageY)
+        let way_id = this._findTagForLongLat(x, y)
         let elem = document.getElementById("pointer-lat-long")
-        elem.textContent = "Lat: " + y + ", Long: " + x
+
+        elem.innerHTML = ""
+
+        if (way_id != -1) {
+            for (let tag of this.data.ways[way_id].tags) {
+                elem.innerHTML += tag
+                elem.innerHTML += "<br>"
+            }
+        }
+        elem.innerHTML += "Lat: " + y + ", Long: " + x
+        this.selected_way_id = way_id
     }
 
     _pixelToLongLat(x, y) {
@@ -180,15 +344,16 @@ class Renderer {
     }
 
     _onMouseLeave(e) {
-        console.log("leave")
         let elem = document.getElementById("overlay")
         elem.style.display = "none"
+        this.selected_way_id = -1
+        window.requestAnimationFrame(this.render_map.bind(this))
+
     }
 
     _onMouseEnter(e) {
         let elem = document.getElementById("overlay")
         elem.style.display = "block"
-        this._update_overlay(e)
     }
 }
 
